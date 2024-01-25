761832301d4fb435367f2cd86700c9a71c2d0512
rakib13x <rakib13x@gmail.com>
2024-01-25 23:13:27 +0600

    Schema for coupon code added

---
+++ src/models/coupon.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  coupon: {
+    type: String,
+    required: [true, "Please enter Coupon code"],
+    unique: true,
+  },
+  amount: {
+    type: Number,
+    required: [true, "Please enter Discount Amount"],
+  },
+});
+
+export const coupon = mongoose.model("Coupon", schema);
